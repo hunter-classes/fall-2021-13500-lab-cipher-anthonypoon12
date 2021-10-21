d7d294da9a48eeafd9837fb0033f70c088760f4e
anthonypoon12 <anthony@poons.us>
2021-10-21 13:29:17 -0400

    added new test in main

--- main.cpp
+++ main.cpp
@@ -11,5 +11,7 @@ int main()
 	addfrequencies(f,"dec.txt");
 	for (int i = 0; i < 26; i++)
 		std::cout<<f[i]<<"\n";
+	std::cout<<solve(" Prudence, indeed, will dictate that Governments long established should not be changed for light and transient causes; and accordingly all experience hath shewn, that mankind are more disposed to suffer, while evils are sufferable, than to right themselves by abolishing the forms to which they are accustomed. ", "dec.txt");
+	std::cout<<"\n";
   return 0;
 }
